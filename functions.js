41a8382bf03e4686d1d0ac1f2e3ffa455adf860a
Edmar <edmarsalvesjr@gmail.com>
2023-05-16 20:21:00 -0300

    Aprendendo funções em JS

---
+++ functions.js
@@ -0,0 +1,28 @@
+/*function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+let res = parimpar(4)
+console.log(res)
+
+function soma (n1, n2) {
+    return n1 + n2
+}
+
+console.log(soma(2, 5))*/
+
+let teste = function parimpar(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+let res = parimpar(111)
+console.log(res)
+console.log(teste)
